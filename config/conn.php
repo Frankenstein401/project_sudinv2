5912de4da24d5d2d64800b025363c0b0d2118687
aly675 <billycaesarrajawali@example.com>
2025-09-12 14:43:57 +0700

    fix: auth in server

--- config/conn.php
+++ config/conn.php
@@ -1,12 +1,11 @@
-    <?php
-    // Konfigurasi database
-    $host = '172.17.1.93';
-    $username = 'root';
-    $password = 'Smkn12jkt';
-    $database = 'database_sudin_dev_test';
-
-    $conn = new mysqli($host, $username, $password, $database);
-    if ($conn->connect_error) {
-        die("Koneksi gagal: " . $conn->connect_error);
-    }
-    ?>
+<?php
+// Konfigurasi database
+$host = '172.17.1.93';
+$username = 'root';
+$password = 'Smkn12jkt';
+$database = 'database_sudin_dev_test';
+$conn = new mysqli($host, $username, $password, $database);
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+?>
